3cb384cb22fa15fcb255fdd5e35cccfc911280d2
espencer <143948310+Stress-In-Coding@users.noreply.github.com>
2023-10-12 22:28:05 +0800

    Update MEMBER.py

--- MEMBER.py
+++ MEMBER.py
@@ -1,3 +1,3 @@
-MEMBER = ["Cydric Vincent Torillo ", "Espencer Madilla ", "Aneza Jamion"]
+MEMBER = ["Cydric Vincent Torillo ", "Espencer Madilla ", "Aneza Jamion","John Angelo Luciano"]
 for MEMBER in MEMBER:
     print(MEMBER)
